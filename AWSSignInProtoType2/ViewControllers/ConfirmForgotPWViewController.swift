bf8e2dae7c55eaeb8a2277c96a85463f25476510
Greg Hughes <dcgg_101@hotmail.com>
2020-03-29 18:45:33 -0600

    added confirmforgotVC

---
+++ AWSSignInProtoType2/ViewControllers/ConfirmForgotPWViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ConfirmForgotPWViewController.swift
+//  AWSSignInProtoType2
+//
+//  Created by Greg Hughes on 3/29/20.
+//  Copyright © 2020 Greg Hughes. All rights reserved.
+//
+
+import UIKit
+
+class ConfirmForgotPWViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
